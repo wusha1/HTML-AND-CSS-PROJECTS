8561e5ae47cf20c114740bd6d94d428bae1255a6
wusha1 <allcompass11@outlook.com>
2026-01-09 19:05:09 -0800

    This is my Pet shop website update!

---
+++ Basic_HTML_and_CSS/JS_Project/Hello_World.js
@@ -0,0 +1,7 @@
+var greeting1 = "Hello, World";
+var greeting2 = "Hello again!";
+var sentence1 = "This sentence will throw an error.";
+
+console.log(greeting1);
+console.log(greeting2);
+console.log(sentence1);
